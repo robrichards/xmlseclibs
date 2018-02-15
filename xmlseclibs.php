e8c22e2bf70f27296cb75807d744cd8f191c9158
Jaime Pérez Crespo <jaime.perez@uninett.no>
2018-02-15 12:55:03 +0100

    Load the new Utils\XPath class in the general file.

--- xmlseclibs.php
+++ xmlseclibs.php
@@ -44,3 +44,4 @@ $xmlseclibs_srcdir = dirname(__FILE__) . '/src/';
 require $xmlseclibs_srcdir . '/XMLSecurityKey.php';
 require $xmlseclibs_srcdir . '/XMLSecurityDSig.php';
 require $xmlseclibs_srcdir . '/XMLSecEnc.php';
+require $xmlseclibs_srcdir . '/Utils/XPath.php';
