0bc3e02c6e1ee59368ca4a9e5ceecdc2746e7565
Jaime Pérez Crespo <jaime.perez@uninett.no>
2018-04-17 13:48:38 +0200

    Add some more tests for the signature algorithm factory.

---
+++ tests/Alg/SimpleSAML/Test/Alg/SignatureAlgorithmFactoryTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace SimpleSAML\XMLSec\Test\Alg;
+
+use SimpleSAML\XMLSec\Alg\Signature\HMAC;
+use SimpleSAML\XMLSec\Alg\Signature\SignatureAlgorithmFactory;
+use SimpleSAML\XMLSec\Constants;
+use SimpleSAML\XMLSec\Exception\RuntimeException;
+use SimpleSAML\XMLSec\Key\PublicKey;
+use SimpleSAML\XMLSec\Key\SymmetricKey;
+
+/**
+ * Tests for SimpleSAML\XMLSec\Alg\Signature\SignatureAlgorithmFactory
+ *
+ * @package SimpleSAML\XMLSec\Test\Alg
+ */
+class SignatureAlgorithmFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @var SymmetricKey */
+    protected $skey;
+
+    /** @var PublicKey */
+    protected $pkey;
+
+
+    public function setUp()
+    {
+        $this->skey = SymmetricKey::generate(16);
+        $this->pkey = PublicKey::fromFile('tests/pubkey.pem');
+    }
+
+
+    /**
+     * Test obtaining the digest algorithm from a signature algorithm.
+     */
+    public function testGetDigestAlgorithm()
+    {
+        $factory = new SignatureAlgorithmFactory([]);
+        $hmac = [
+            Constants::SIG_HMAC_SHA1      => Constants::DIGEST_SHA1,
+            Constants::SIG_HMAC_SHA224    => Constants::DIGEST_SHA224,
+            Constants::SIG_HMAC_SHA256    => Constants::DIGEST_SHA256,
+            Constants::SIG_HMAC_SHA384    => Constants::DIGEST_SHA384,
+            Constants::SIG_HMAC_SHA512    => Constants::DIGEST_SHA512,
+            Constants::SIG_HMAC_RIPEMD160 => Constants::DIGEST_RIPEMD160,
+        ];
+
+        $rsa = [
+            Constants::SIG_RSA_SHA1      => Constants::DIGEST_SHA1,
+            Constants::SIG_RSA_SHA224    => Constants::DIGEST_SHA224,
+            Constants::SIG_RSA_SHA256    => Constants::DIGEST_SHA256,
+            Constants::SIG_RSA_SHA384    => Constants::DIGEST_SHA384,
+            Constants::SIG_RSA_SHA512    => Constants::DIGEST_SHA512,
+            Constants::SIG_RSA_RIPEMD160 => Constants::DIGEST_RIPEMD160,
+        ];
+
+        foreach ($hmac as $signature => $digest) {
+            $factory->getAlgorithm($signature, $this->skey);
+            $this->assertEquals($digest, $factory->getDigestAlgorithm());
+        }
+
+        foreach ($rsa as $signature => $digest) {
+            $factory->getAlgorithm($signature, $this->pkey);
+            $this->assertEquals($digest, $factory->getDigestAlgorithm());
+        }
+    }
+
+
+    /**
+     * Test for unsupported algorithms.
+     *
+     * @expectedException RuntimeException
+     */
+    public function testGetUnknownAlgorithm()
+    {
+        $factory = new SignatureAlgorithmFactory([]);
+        $factory->getAlgorithm('Unknown alg', $this->skey);
+    }
+
+
+    /**
+     * Test for blacklisted algorithms.
+     *
+     * @expectedException RuntimeException
+     */
+    public function testBlacklistedAlgorithm()
+    {
+        $factory = new SignatureAlgorithmFactory([Constants::SIG_RSA_SHA1]);
+        $this->assertInstanceOf(
+            '\SimpleSAML\XMLSec\Alg\Signature\HMAC',
+            $factory->getAlgorithm(Constants::SIG_HMAC_SHA1, $this->skey)
+        );
+        $factory->getAlgorithm(Constants::SIG_RSA_SHA1, $this->pkey);
+    }
+}
