eadb8f66d9fb3507d165e3ff1fbbe1b7bf153209
Rob Richards <rrichards@cdatazone.org>
2018-11-15 08:39:04 -0500

    Fix version

--- xmlseclibs.php
+++ xmlseclibs.php
@@ -37,7 +37,7 @@
  * @author    Robert Richards <rrichards@cdatazone.org>
  * @copyright 2007-2018 Robert Richards <rrichards@cdatazone.org>
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   2.0.2
+ * @version   2.1.0
  */
 
 $xmlseclibs_srcdir = dirname(__FILE__) . '/src/';
