f1ef0709646284ecd08bbe01f963035f5c90972d
Rob Richards <rrichards@cdatazone.org>
2018-11-15 07:01:26 -0500

    Back to dev

--- xmlseclibs.php
+++ xmlseclibs.php
@@ -37,7 +37,7 @@
  * @author    Robert Richards <rrichards@cdatazone.org>
  * @copyright 2007-2018 Robert Richards <rrichards@cdatazone.org>
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version   3.0.3-dev
+ * @version   3.0.4-dev
  */
 
 $xmlseclibs_srcdir = dirname(__FILE__) . '/src/';
