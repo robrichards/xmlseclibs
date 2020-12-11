5a446461fa6cdc768227a21a2053799d286e501c
Tobias Assmann <tobias.assmann@ecsec.de>
2020-12-11 11:09:45 +0100

    fix autoload require, as dep is not found in local vendor dir if lib is used in composer context

--- src/XMLSecurityKey.php
+++ src/XMLSecurityKey.php
@@ -1,7 +1,9 @@
 <?php
 namespace RobRichards\XMLSecLibs;
 
-require __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
+}
 
 use DOMElement;
 use Exception;
